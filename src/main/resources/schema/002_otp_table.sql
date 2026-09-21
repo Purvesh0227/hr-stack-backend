2310d387e9cfbf26a65ba951c248b72170a7c3b5
React1 <purvesh0207@gmail.com>
2026-09-21 11:31:27 +0530

    TASK-034 — creates .env.example containing variable names

--- src/main/resources/schema/002_otp_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE otp (
-                     uuid UUID NOT NULL PRIMARY KEY,
-                     created_by UUID NOT NULL,
-                     created_on BIGINT NOT NULL,
-                     otp_date BIGINT NOT NULL,
-                     department VARCHAR(255) NOT NULL,
-                     expires_on BIGINT NOT NULL,
-                     otp VARCHAR(6) NOT NULL
-);
